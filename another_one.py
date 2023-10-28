0c44a51216e0e0da5a42f8019cb598497d6ca4cb
Vanessa Ibanez <vibanez1@terpmail.umd.edu>
2023-10-27 21:46:09 -0400

    added printing_func_two

--- another_one.py
+++ another_one.py
@@ -4,3 +4,9 @@ def printing_func(x, y):
 anwser = printing_func(3,5)
 print(anwser)
 print('yellow')
+
+def printing_func_two(b, a):
+    c = a + b
+    return c
+anwser_two = printing_func_two(7,8)
+print(anwser_two)
